2648082ea839e2450f8e6d2f4aab84cc09d25e2b
Parker Lusk <parkerclusk@gmail.com>
2018-02-01 20:19:32 -0700

    added discoveryf3 time example

---
+++ boards/discoveryf3/examples/time/main.cpp
@@ -0,0 +1,33 @@
+#include <led.h>
+#include <vcp.h>
+
+
+int main()
+{
+  systemInit();
+
+  LED info;
+  LED warn;
+  info.init(GPIOE, GPIO_Pin_13);
+  warn.init(GPIOE, GPIO_Pin_12);
+
+  VCP vcp;
+  vcp.connect_to_printf();
+
+  printf("\n**** Time Example ****\n\n");
+
+  info.off();
+  warn.on();
+
+
+  int i = 0;
+  while(1)
+  {
+    warn.toggle();
+    info.toggle();
+
+    printf("time = %d s, %d ms, %d us\n", i++, millis(), (uint32_t)micros());
+
+    delay(1000);
+  }
+}
