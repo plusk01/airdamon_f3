89d4c6c60af6c3063505292c694ba030dfe66f82
Parker Lusk <parkerclusk@gmail.com>
2019-04-28 18:22:59 -0400

    fixes #6, imu and vcp inter-dependence

--- boards/beecore/board.c
+++ boards/beecore/board.c
@@ -10,6 +10,9 @@ void board_init(void)
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
 
+  // GPIO EXTI (for imu)
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
   // Enable clock to DMA1, an AHB peripheral
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
   // RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
--- boards/discoveryf3/board.c
+++ boards/discoveryf3/board.c
@@ -5,6 +5,9 @@ void board_init(void)
   // Initialize the timing and other parts of the airdamon system
   systemInit();
 
+  // GPIO EXTI (for imu)
+  // RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
 
